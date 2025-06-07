559ced9b45fcf3c1746017b890a5c8929cfb2ce5
hamzainvalid <you@example.com>
2025-06-07 16:39:18 +0200

    added more voices and enhanced the script

---
+++ templates/11labs_voices.py
@@ -0,0 +1,33 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TEST_EL_API = os.getenv('TEST_EL_API')
+NN_EL_API = os.getenv('NN_EL_API')
+PERSONAL_EL_API = os.getenv('PERSONAL_EL_API')
+
+def testing_api(voice):
+    voice = voice
+    api_key = NN_EL_API
+    voice_id =  voice
+
+    response = requests.post(
+        f"https://api.elevenlabs.io/v1/text-to-speech/{voice_id}",
+        headers={
+            "xi-api-key": api_key,
+            "Content-Type": "application/json"
+        },
+        json={
+            "text": '',
+            "model_id": "eleven_multilingual_v2"
+        }
+    )
+
+    if response.status_code == 200:
+        print('Audio works for api')
+    else:
+        print('no api for this voice')
+
+testing_api('1rnYMVDXZksVr6x7pZPX')
